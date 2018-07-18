3c29a71d1245d00f5c7f52fa97429c7639889802
Mica Eldridge <mica.eldridge1@gmail.com>
2018-07-18 14:27:27 -0700

    added agent-side key authentication

--- agent/simple_ping.bash
+++ agent/simple_ping.bash
@@ -9,7 +9,21 @@ else
     CACERT=
 fi
 
+if [ -n "$SIMPLE_PING_KEY" ] ; then
+    SIMPLE_PING_KEY="&key=$SIMPLE_PING_KEY"
+else
+    SIMPLE_PING_KEY=
+fi
+
+url="https://$MANAGER_IP:$MANAGER_PORT/simple_ping?public_ip=$my_public_ip$SIMPLE_PING_KEY"
+
+response=$(
 curl \
+    "$url" \
     --silent \
-    "https://$MANAGER_IP:$MANAGER_PORT/simple_ping?public_ip=$my_public_ip" \
     $CACERT
+)
+
+if [ "$response" = "404" ] ; then
+    echo "failed to connect to manager"
+fi
