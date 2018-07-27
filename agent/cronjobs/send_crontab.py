773eddbda30eb45f18df2e163b97662b134b4751
mica <mica.eldridge1@gmail.com>
2018-07-26 18:36:02 -0700

    made it possible to specify a port for the manager

--- agent/cronjobs/send_crontab.py
+++ agent/cronjobs/send_crontab.py
@@ -14,22 +14,26 @@ this_dir = os.path.dirname(os.path.abspath(__file__))
 if this_dir not in sys.path:
     sys.path.insert(0, this_dir)
 
-from config import user, manager, cronjob_repo_path_on_manager
+from config import user, manager, manager_port, cronjob_repo_path_on_manager
 
 
 def run_main():
     args = parse_cl_args()
 
+    port = '-p ' + manager_port if manager_port else ''
+    user = user+'@' if user else ''
+
     command = """
-        \ncrontab -l | ssh {user}{manager} 'cat > {cronjob_repo_path_on_manager}/crontab.txt'
-        \nssh {user}{manager} <<EOF
+        \ncrontab -l | ssh {user}{manager} {port} 'cat > {cronjob_repo_path_on_manager}/crontab.txt'
+        \nssh {user}{manager} {port} <<EOF
             cd {cronjob_repo_path_on_manager}
             git add crontab.txt
             git commit -m "autocommit from host_watcher/agent/cronjobs/send_crontab.py"
         \nEOF\n
     """.format(
-        user='{}@'.format(user) if user else '',
+        user=user,
         manager=manager,
+        port=port,
         cronjob_repo_path_on_manager=cronjob_repo_path_on_manager,
     )
     if args.dry_run:
